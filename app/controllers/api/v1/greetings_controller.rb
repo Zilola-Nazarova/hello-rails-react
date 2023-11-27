4e69333ac63d93ae86fc7e7bf0d81abc2b0fe1df
zilola <zilolanazarova1994@gmail.com>
2023-11-28 03:50:25 +0500

    Move greetings_controller to the api/v1 folder

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,4 +1,4 @@
-class GreetingsController < ActionController
+class Api::V1::GreetingsController < ApplicationController
   def random_greeting
     @greeting = Greeting.find(Greeting.pluck(:id).sample)
     render json: @greeting, status: :ok
